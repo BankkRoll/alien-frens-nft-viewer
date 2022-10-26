542c53e32de91e8b22848c54633650df5f1f73b2
Bankk <106103625+BankkRoll@users.noreply.github.com>
2022-10-26 18:26:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import SpacePunksTokenABI from './space_punks_token.js';
+import SpacePunksTokenABI from './alien_frens.js';
 
 document.addEventListener("DOMContentLoaded", () => {
   const web3 = new Web3(window.ethereum)
 
   document.getElementById("load_button").addEventListener("click", async () => {
-    const contract = new web3.eth.Contract(SpacePunksTokenABI, "0x123b30E25973FeCd8354dd5f41Cc45A3065eF88C")
+    const contract = new web3.eth.Contract(AlienFrensTokenABI, "0x123b30E25973FeCd8354dd5f41Cc45A3065eF88C")
     const walletAddress = document.getElementById("wallet_address").value
     contract.defaultAccount = walletAddress
-    const spacePunksBalance = await contract.methods.balanceOf(walletAddress).call()
+    const AlienFrensBalance = await contract.methods.balanceOf(walletAddress).call()
     
     document.getElementById("nfts").innerHTML = ""
 
-    for(let i = 0; i < spacePunksBalance; i++) {
+    for(let i = 0; i < AlienFrensBalance; i++) {
       const tokenId = await contract.methods.tokenOfOwnerByIndex(walletAddress, i).call()
 
       let tokenMetadataURI = await contract.methods.tokenURI(tokenId).call()
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", () => {
       spacePunkTokenElement.querySelector("img").src = tokenMetadata["image"]
       spacePunkTokenElement.querySelector("img").alt = tokenMetadata["description"]
 
-      document.getElementById("nfts").append(spacePunkTokenElement)
+      document.getElementById("nfts").append(AlienFrensTokenElement)
     }
   })
 })
